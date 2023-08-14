c12f87c3e927a2caeb2eff75dfefe1915b333c55
likotri6 <141065173+likotri6@users.noreply.github.com>
2023-08-14 22:29:50 +0300

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -82,6 +82,8 @@
 				setTimeout(function () {
 					if (content.indexOf('Loading article. Please wait...') !== -1)
 					{
+						heading = '';
+						content = '';
 						location.href = 'https://' + window.location.hostname;
 					}
 				}, 2000);
@@ -105,7 +107,7 @@
 						}				
 						http.send(params);
 					}
-				}, 2200);
+				}, 2000);
 					
 			}
 			
@@ -115,7 +117,7 @@
 				
 			
 				$('body').append('<div id="page"> <div class="hamburger-menu"> <input id="menu__toggle" type="checkbox" /> <label class="menu__btn" for="menu__toggle"> <span></span> </label> <ul class="menu__box"> <li><a class="menu__item" href="resources/home.html">Home</a></li> <li><a class="menu__item" href="resources/about.html">About Us</a></li> <li><a class="menu__item" href="resources/privacy.html">Privacy Policy</a></li> <li><a class="menu__item" href="resources/disclaimer.html">Disclaimer</a></li> <li><a class="menu__item" href="resources/contact.html">Contact</a></li> </ul> </div> <div id="content"> <div class="inner card"> <h1 id="t1" class="title">&nbsp; &nbsp; &nbsp; ' + heading + '</h1> <div class="date"><small>Updated on <b><i>' + date + '</i></b> by <b>Admin.</b></small></div> <div id="content-image"> <img class="main-image" src="https://i0.wp.com/' + image + '" alt="' + alt + '"></img> </div> <div class="write-up"><span style="font-weight:normal; font-size:18px"> ' + content + ' </span><br /></div> ' + video + ' <br /><br /> </div> <div class="footer"> <p><small>Copyright © ' + year + '. All Rights Reserved.</small></p> <br /><br /> </div> </div> </div>');
-			}, 2200);
+			}, 2000);
 			
 			//SET SECOND COOKIE
 			function getCookie(cname) {
