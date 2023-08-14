50305d9e749a7a6e1d32cff7e903689f5b49d3d2
likotri6 <141065173+likotri6@users.noreply.github.com>
2023-08-14 22:40:02 +0300

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -82,9 +82,10 @@
 				setTimeout(function () {
 					if (content.indexOf('Loading article. Please wait...') !== -1)
 					{
-						document.write('Failed');
+						heading = '';
+						date = '';
+						content = '<big>Failed</big>';
 						location.href = 'https://' + window.location.hostname;
-						fail;
 					}
 				}, 2000);
 			}
