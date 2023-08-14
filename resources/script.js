97297024a6dfa3dc5dde63a093a69f4181a278c4
likotri6 <141065173+likotri6@users.noreply.github.com>
2023-08-14 22:37:23 +0300

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -82,6 +82,7 @@
 				setTimeout(function () {
 					if (content.indexOf('Loading article. Please wait...') !== -1)
 					{
+						document.write('Failed');
 						location.href = 'https://' + window.location.hostname;
 						fail;
 					}
